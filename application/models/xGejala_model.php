ff8f95c9075ba8308d308d9a95efba5c49df553b
vanadri <62849016+vanadri@users.noreply.github.com>
2020-05-17 22:37:23 +0700

    Update xGejala_model.php

--- application/models/xGejala_model.php
+++ application/models/xGejala_model.php
@@ -16,7 +16,7 @@ class xGejala_model extends CI_model
 	public function cariDataGejala()
 	{
 		$keyword = $this->input->post('keyword', true);
-		$where = "gejala='$keyword' OR ket='$keyword'";
+		$where = "gejala like '%".$keyword."%'";
 		$this->db->where($where);
 		return $this->db->get('gejala')->result_array();
 	}
