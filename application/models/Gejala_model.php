54f4bfd3caba62570b341f6df0ce1008fcf774cc
vanadri <62849016+vanadri@users.noreply.github.com>
2020-05-17 17:22:37 +0700

    Create Gejala_model.php

---
+++ application/models/Gejala_model.php
@@ -0,0 +1,48 @@
+<?php
+class Gejala_model extends CI_model
+{
+
+	public function getAllGejala()
+	{
+		return $this->db->get('gejala')->result_array();
+	}
+
+	public function tambahDataGejala()
+	{
+		$data = [
+			"gejala" => $this->input->post('gejala', true),
+			"ket" => $this->input->post('ket', true),
+		];
+		return $this->db->insert('gejala', $data);
+	}
+
+	public function hapusDataGejala($id)
+	{
+		$this->db->where('id', $id);
+		return $this->db->delete('gejala');
+	}
+
+	public function getGejalaById($id)
+	{
+		$this->db->where('id', $id);
+		return $this->db->get('gejala')->row_array();
+	}
+
+	public function ubahDataGejala($id)
+	{
+		$data = [
+			"gejala" => $this->input->post('gejala', true),
+			"ket" => $this->input->post('ket', true),
+		];
+		$this->db->where('id', $id);
+		return $this->db->update('gejala', $data);
+	}
+
+	public function cariDataGejala()
+	{
+		$keyword = $this->input->post('keyword', true);
+		$where = "gejala='$keyword' OR ket='$keyword'";
+		$this->db->where($where);
+		return $this->db->get('gejala')->result_array();
+	}
+}
