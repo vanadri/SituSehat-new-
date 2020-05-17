f709b453de69610e673771452dde7745967355ea
vanadri <62849016+vanadri@users.noreply.github.com>
2020-05-17 22:37:52 +0700

    Update xHidupSehat_model.php

--- application/models/xHidupSehat_model.php
+++ application/models/xHidupSehat_model.php
@@ -16,7 +16,7 @@ class xHidupSehat_model extends CI_model
 	public function cariDataHidupSehat()
 	{
 		$keyword = $this->input->post('keyword', true);
-		$where = "sehat='$keyword' OR dampak='$keyword'";
+		$where = "sehat like '%".$keyword."%' OR dampak like '%".$keyword."%'";
 		$this->db->where($where);
 		return $this->db->get('sehat')->result_array();
 	}
